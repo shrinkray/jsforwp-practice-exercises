39b260d398df84928e1b7d8448f1259b3c9863f0
Greg <info@shrinkray.net>
2019-01-15 17:50:24 -0500

    Tasks 6 & 7 creating array and for in loop with array data

--- week-1-js-basics/starter.js
+++ week-1-js-basics/starter.js
@@ -27,6 +27,7 @@ console.log('Hello ' + thisIsMe.last);
 
 // 4. Create a boolean variable called loggedIn (set to either true or false)
 
+// Assign variables for next conditional
 var loggedIn = true;
     isLoggedIn = 'Logged In';
     notLoggedIn = 'Please login';
@@ -38,9 +39,19 @@ loggedIn ? console.log(isLoggedIn) : console.log(notLoggedIn);
 
 // 6. Create an array called ids filled with numbers that could represent post ids
 
+var id = [ 12, 23, 47, 52, 78 ];
+
+
 // 7. Loop over the ids and log them all out
 
+// This is new to me. Now using example showing this 'for in' combo
+for ( var i in id ) {
+    console.log(id[i]);
+}
+
 // 8. Create an array of post objects.  Include an id, title and content property on each object
+
+
 
 // 9. Loop over the array of posts and log out the title of each one
 
