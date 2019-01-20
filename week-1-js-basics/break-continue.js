79b44fdb4b686b700effdceca33f0d770b3999d0
Greg <info@shrinkray.net>
2019-01-20 15:36:20 -0500

    Examples demonstrating break and continue

---
+++ week-1-js-basics/break-continue.js
@@ -0,0 +1,52 @@
+/**
+ * Break & Continue Statements
+ * 
+ * Beak allows to exit
+ * In a switch statement, break confirms the condtion has been met and forces out of conditional
+ * 
+ * Continue to stay in
+ * 
+ */
+
+// Switch example
+
+switch ( 'user.role' ) {
+    case 'admin':
+        console.log( 'Show Admin');
+        break;
+    case 'editor': 
+        console.log( 'Show all posts' );
+        break;
+
+        // ... 
+
+    default: 
+      console.log( 'Contact Admin' );
+
+    
+}
+
+// Loops
+
+var categories = [ 'Javascript', 'WordPress', 'API', 'React', 'JSON' ],
+    max = categories.length;
+
+    for ( var i = 0; i < max; i++ ) {
+        if ( 'API' === categories[i] ) break; // exits out of loop when condition met
+            
+        console.log( categories[i] ); // placing this before condtion will print what we are looking for. 
+
+    }
+
+var categories = ['Javascript', 'WordPress', 'API', 'React', 'JSON'],
+    max = categories.length;
+
+for (var i = 0; i < max; i++) {
+    console.log(categories[i]);
+
+    // I don't have a clear understanding of this one
+    if ('API' === categories[i]) continue; // continues when condition met
+
+   
+
+}
---
+++ week-1-js-basics/compare-loops.js
@@ -0,0 +1,18 @@
+/**
+ * Examples
+ * 
+ */
+
+ // use a decision table
+
+ /**
+  * Table to test which loop suits the need
+  * 
+  * Checks for initial Expressions         | For
+  * Checks for Increment expressions       | For |  -      |  -
+  * checks condition before executing loop | For | While   |  -
+  * checks conditions after executing loop |  -  |   -     | Do While
+  * 
+  * 
+  */
+
